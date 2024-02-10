8c7a7774faa9c77f0d44f6b36434dc57b9e12167
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 01:38:22 -0600

    debug / bug fix

--- Webserver/API/getteams.js
+++ Webserver/API/getteams.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-let teams = require('./Data/teams.json');
+let teams = null;
 router.get("/", (req, res, next) =>{
     teams = require('./Data/teams.json');
     res.send(teams);
+    teams = null;
     res.status(200);
 });
 
