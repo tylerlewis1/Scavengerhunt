1d3315c9545090487e6e182d44cb21f634b75c30
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 13:58:23 -0600

    added file function

--- Webserver/API/rmteam.js
+++ Webserver/API/rmteam.js
@@ -6,10 +6,12 @@ const router = express.Router();
 let datafile = null;
 let currentdata = null;
 let data = [];
-
+let imagedata = [];
 router.post("/", (req, res, next) =>{
     datafile = fs.readFileSync("./API/Data/teams.json");
+    imagedatafile = fs.readFileSync("./API/Data/images.json");
     let currentdata = JSON.parse(datafile);
+    let currentimagedata = JSON.parse(imagedatafile);
     //old data push
     for(i = 0; i < currentdata.length; i++){
         if(req.body.id != currentdata[i].teamid) {
@@ -20,12 +22,20 @@ router.post("/", (req, res, next) =>{
                 teamid: currentdata[i].teamid,
                 teamphoto: currentdata[i].teamphoto
             });
+            iamgedata.push({
+                teamname: currentdata[i].teamname,
+                teamid: currentdata[i].teamid,
+                images: currentimagedata[i].images
+            });
         }
     }
     // write data
     let stringjson = JSON.stringify(data, null, 2);
+    let stringjson1 = JSON.stringify(imagedata, null, 2);
     fs.writeFileSync("./API/Data/teams.json", stringjson);
+    fs.writeFileSync("./API/Data/images.json", stringjson1);
     data = [];
+    imagedata = [];
     res.send("Done");
     res.status(200);
 });
