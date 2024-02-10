660674a2f63e7dbb5dc758de71ccc4af13ff9e23
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 01:57:06 -0600

    debug

--- Webserver/API/getteams.js
+++ Webserver/API/getteams.js
@@ -2,9 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 let teams = null;
+let data = [];
 router.get("/", (req, res, next) =>{
     teams = fs.readFileSync('./API/Data/teams.json');
-    res.send(teams.toJSON);
+    for(i = 0; i < teams.length; i++){
+        data.push(teams[i]);
+    }
+    res.send(data);
     teams = null;
     res.status(200);
 });
