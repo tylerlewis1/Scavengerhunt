a1ae141bd378f5b3e5a642299e8c1e84ce03858b
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 13:48:01 -0600

    added image log

--- Webserver/API/addteam.js
+++ Webserver/API/addteam.js
@@ -4,12 +4,16 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 let datafile = null;
+let iamgedatafile = null;
 let currentdata = null;
 let data = [];
+let imagedata = [];
 
 router.post("/", (req, res, next) =>{
     datafile = fs.readFileSync("./API/Data/teams.json");
+    iamgedatafile = fs.readFileSync("./API/Data/images.json");
     let currentdata = JSON.parse(datafile);
+    let currentimagedata = JSON.parse(iamgedatafile);
     //old data push
     for(i = 0; i < currentdata.length; i++){
          data.push({
@@ -18,6 +22,11 @@ router.post("/", (req, res, next) =>{
             locfound: currentdata[i].locfound,
             teamid: currentdata[i].teamid,
             teamphoto: currentdata[i].teamphoto
+         });
+         imagedata.push({
+            teamname: currentdata[i].teamname,
+            teamid: currentdata[i].teamid,
+            images: currentimagedata[i].images
          });
     }
     //new data push
@@ -28,9 +37,16 @@ router.post("/", (req, res, next) =>{
         teamid: (currentdata.length + 1),
         teamphoto: req.body.teamphoto
      });
+     imagedata.push({
+        teamname: req.body.teamname,
+        teamid: (currentdata.length + 1),
+        teamphoto: req.body.teamphoto
+     });
     // write data
     let stringjson = JSON.stringify(data, null, 2);
+    let stringjson1 = JSON.stringify(imagedata, null, 2);
     fs.writeFileSync("./API/Data/teams.json", stringjson);
+    fs.writeFileSync("./API/Data/images.json", stringjson1);
     data = [];
     res.send("Done");
     res.status(200);
