d351ac570005a1ac081227c8fa4e26349184c171
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 01:16:51 -0600

    added endpoint

---
+++ Webserver/API/addteam.js
@@ -0,0 +1,38 @@
+const { Console } = require('console');
+const { json } = require('express');
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+let datafile = null;
+let currentdata = null;
+let data = [];
+
+router.post("/", (req, res, next) =>{
+    datafile = fs.readFileSync("./API/Data/teams.json");
+    let currentdata = JSON.parse(datafile);
+    //old data push
+    for(i = 0; i < currentdata.length; i++){
+         data.push({
+            teamname: currentdata[i].teamname,
+            players: currentdata[i].players,
+            locfound: currentdata[i].locfound,
+            teamid: currentdata[i].teamid,
+            teamphoto: currentdata[i].teamphoto
+         });
+    }
+    //new data push
+    data.push({
+        teamname: req.body.teamname,
+        players: req.body.players,
+        locfound: [],
+        teamid: currentdata.length,
+        teamphoto: req.body.teamphoto
+     });
+    // write data
+    let stringjson = JSON.stringify(data, null, 2);
+    fs.writeFileSync("./API/Data/teams.json", stringjson);
+    data = [];
+    res.send("team added");
+    res.status(200);
+});
+module.exports = router;
