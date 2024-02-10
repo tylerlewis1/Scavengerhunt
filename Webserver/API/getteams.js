a6e1e393ce7f088dca7bcd0097b1f933ea48d45f
Tyler Lewis <lewis8055@uwlax.edu>
2024-02-10 01:53:51 -0600

    Slight path fix

--- Webserver/API/getteams.js
+++ Webserver/API/getteams.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const router = express.Router();
 let teams = null;
 router.get("/", (req, res, next) =>{
-    teams = fs.readFileSync('./Data/teams.json');
+    teams = fs.readFileSync('./API/Data/teams.json');
     res.send(teams.toJSON);
     teams = null;
     res.status(200);
